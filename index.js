f6989ae0e1c1e5cb01d25593b1b2f8fd84a80ece
FilipeFariaas <filipefarias117@gmail.com>
2022-03-05 12:16:53 -0300

    finish animations

--- index.js
+++ index.js
@@ -10,26 +10,39 @@ const aboutTitle = aboutSection.querySelector(`h1`);
 const controller = new ScrollMagic.Controller();
 
 // * SCENES -> WHERE THE ANIMATION WILL HAPPEN
-const scene = new ScrollMagic.Scene({
+// VIDEO ANIMATION
+let videoScene = new ScrollMagic.Scene({
 	duration: 5000, // THE SAME AS THE VIDEO DURATION
 	triggerElement: introSection, // WHICH ELEMENT TRIGGER THE ANIMATION
 	triggerHook: 0, // WHERE THE SENCE STARTS -> 0 = top: 0
 })
-	.addIndicators() // SHOWS START/END INDICATORS
+	// .addIndicators() // SHOWS START/END INDICATORS
 	.setPin(introSection) // THE PAGE WON'T SCROLL UNTIL THE SCENE ENDS
 	.addTo(controller);
 
-// * VIDEO ANIMATION
-let accelAmount = 0.1; // AS THE VIDEO'S FRAME GOES FORWARD, WHEN ITS STOP, THAT APPLIES A "EASE-OUT" ANIMATION
+// TITLE ANIMATION
+const introTitleAnimation = TweenMax.fromTo(introTitle, 4, { opacity: 1 }, { opacity: 0 });
+
+let introTitleScene = new ScrollMagic.Scene({
+	duration: 4000,
+	triggerElement: introSection,
+	triggerHook: 0,
+})
+	// .addIndicators() // SHOWS START/END INDICATORS
+	.setTween(introTitleAnimation)
+	.addTo(controller);
+
+let accelAmount = 0.05; // AS THE VIDEO'S FRAME GOES FORWARD, WHEN ITS STOP, THAT APPLIES A "EASE-OUT" ANIMATION
 let scrollPosition = 0;
 let delay = 0;
 
-scene.on(`update`, (e) => {
-	scrollPosition = e.scrollPos / 1000; // scrollPos COMES FROM THE "e" EVENT FROM THE ".on" LISTENER | NOTHING TO DO WITH THE "scrollPosition" VARIABLE
+videoScene.on(`update`, (e) => {
+	scrollPosition = e.scrollPos / 1000; // scrollPos COMES FROM THE "e" EVENT FROM THE "on" LISTENER | NOTHING TO DO WITH THE "scrollPosition" VARIABLE
 });
 
 setInterval(() => {
 	delay += (scrollPosition - delay) * accelAmount;
 
-	video.currentTime = delay;
+	video.currentTime = scrollPosition;
+	// video.currentTime = delay; // UNCOMMENT TO ADD DELAY
 }, 41.6); // 41 IS THE FRAME RATE, ADJUST ACCORDINLY -> 1 SECOND DIVIDED BY THE FRAME RATE, IN THIS CASE 24
